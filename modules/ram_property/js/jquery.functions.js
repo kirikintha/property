b1e7d516992fb23f72fa3287e308f11753b6a40b
Paul T. Huntsberger <Paul@paul-t-huntsbergers-macbook-pro-15.local>
2010-12-11 15:09:48 -0700

    1. If we cannot cache through the UI set a Drupal message. 2. Refactored the processOrphans function so that it will not produce memory problems when running large batches. 3. If for some reason the query using cURL fails or has a timeout, then we produce a php error. Fixed that. 4. Changed the js interface to include another comit for processing orphans. 5. $newCacheProperties produces a lot of over head in the entire process, so I turned this into cached data and move _processOrphans out of the propertyCaching analysis. Fixes a known problem with large recordsets exhausting the memory when uses a large number of node_loads in for_each loop. The only time a node really needs to load is when we are deleting a property.

--- modules/ram_property/js/jquery.functions.js
+++ modules/ram_property/js/jquery.functions.js
@@ -13,7 +13,7 @@ if (Drupal.jsEnabled) {
      */
     $("#edit-test-connection").bind( 'click', function() {
     
-      _commit( $(this), null, '_ram_property_test_connection', '#test-progress', 0, 100, 'Starting Test', 'Test completed, just a moment while we update the module configuration...' );
+      _commit( $(this), null, '_ram_property_test_connection', '#test-progress', 10, 100, 'Starting Test', 'Test completed, just a moment while we update the module configuration...' );
       
       return false;
     
@@ -21,7 +21,7 @@ if (Drupal.jsEnabled) {
     
     $("#edit-property-connection-tokenize").bind( 'click', function() {
     
-      _commit( $(this), $("#edit-property-tokens").val(), '_ram_property_generate_tokens', '#token-progress', 0, 100, 'Generating Tokens', 'Token Generation Completed' );
+      _commit( $(this), $("#edit-property-tokens").val(), '_ram_property_generate_tokens', '#token-progress', 10, 100, 'Generating Tokens', 'Token Generation Completed' );
       
       return false;
     
@@ -29,12 +29,14 @@ if (Drupal.jsEnabled) {
     
     $("#edit-property-cache-all").bind( 'click', function() { //cache all from server
       
-      _commit( $(this), null, '_ram_property_retrieve_new_cache', '#cache-progress', 0, 33, 'Retrieving New Cache From Server.', 'New Cache retrieved.' );
+      _commit( $(this), null, '_ram_property_retrieve_new_cache', '#cache-progress', 10, 25, 'Retrieving New Cache From Server.', 'New Cache retrieved.' );
       
-      _commit( $(this), null, '_ram_property_analyze_new_cache', '#cache-progress', 33, 66, 'Analyzing New Cache.', 'Analysis completed.' );
+      _commit( $(this), null, '_ram_property_analyze_new_cache', '#cache-progress', 25, 50, 'Analyzing New Cache.', 'Analysis completed.' );
       
-      _commit( $(this), null, '_ram_property_analyze_new_images', '#cache-progress', 66, 100, 'Analyzing New Cached Images.', 'Analysis completed.' );
+      _commit( $(this), null, '_ram_property_analyze_new_images', '#cache-progress', 50, 75, 'Analyzing New Cached Images.', 'Analysis completed.' );
       
+      _commit( $(this), null, '_ram_property_process_orphans', '#cache-progress', 75, 100, 'Processing Orphans.', 'Analysis completed.' );
+
       return false;
     
     } );
